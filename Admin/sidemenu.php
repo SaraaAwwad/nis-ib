30c148bb295061c1a1126ff1122a6da6d18a0310
Amirag96 <amira1504361@miuegypt.edu.eg>
2018-03-07 10:11:44 +0200

    sidebar

--- Admin/sidemenu.php
+++ Admin/sidemenu.php
@@ -1,79 +1,59 @@
-<aside>
+      <aside>
           <div id="sidebar"  class="nav-collapse ">
               <!-- sidebar menu start-->
-              <ul class="sidebar-menu" id="nav-accordion">
+                    <ul class="sidebar-menu" id="nav-accordion">
               
-              	  <p class="centered"><a href="profile.php"><img src="assets/img/ui-sam.jpg" class="img-circle" width="60"></a></p>
-              	  <h5 class="centered">Marcel Newman</h5>
-              	  	
+                  <p class="centered"><a href="profile.html"><img src="assets/img/ui-sam.jpg" class="img-circle" width="60"></a></p>
+                  <h5 class="centered">Admin</h5>
+                    
                   <li class="mt">
-                      <a class="active" href="index.php">
+                      <a href="index.html">
                           <i class="fa fa-dashboard"></i>
                           <span>Dashboard</span>
                       </a>
                   </li>
 
-                  <li class="sub-menu">
-                      <a href="javascript:;" >
-                          <i class="fa fa-desktop"></i>
-                          <span>UI Elements</span>
-                      </a>
-                      <ul class="sub">
-                          <li><a  href="general.php">General</a></li>
-                          <li><a  href="buttons.php">Buttons</a></li>
-                          <li><a  href="panels.php">Panels</a></li>
+                    <li class="sub-menu dcjq-parent-li">
+                      <a class="active dcjq-parent" href="javascript:;">
+                          <i class="fa fa-book"></i>
+                          <span>Pages</span>
+                        <span class="dcjq-icon"></span></a>
+                      <ul class="sub" style="display: block;">
+                          <li><a href="view_pages.html">View Pages</a></li>
+                          <li class="active"><a href="add_page.html">Add Page</a></li>
                       </ul>
                   </li>
 
-                  <li class="sub-menu">
-                      <a href="javascript:;" >
-                          <i class="fa fa-cogs"></i>
-                          <span>Components</span>
-                      </a>
-                      <ul class="sub">
-                          <li><a  href="calendar.php">Calendar</a></li>
-                          <li><a  href="gallery.php">Gallery</a></li>
-                          <li><a  href="todo_list.php">Todo List</a></li>
+                    <li class="sub-menu dcjq-parent-li">
+                      <a class="dcjq-parent" href="javascript:;">
+                          <i class="fa fa-bars"></i>
+                          <span>Courses</span>
+                        <span class="dcjq-icon"></span></a>
+                      <ul class="sub" style="display: block;">
+                          <li><a href="view_courses.html">View Courses</a></li>
+                          <li><a href="add_course.html">Add Course</a></li>
                       </ul>
                   </li>
-                  <li class="sub-menu">
-                      <a href="javascript:;" >
-                          <i class="fa fa-book"></i>
-                          <span>Extra Pages</span>
-                      </a>
-                      <ul class="sub">
-                          <li><a  href="blank.php">Blank Page</a></li>
-                          <li><a  href="login.php">Login</a></li>
-                          <li><a  href="lock_screen.php">Lock Screen</a></li>
+
+                       <li class="sub-menu dcjq-parent-li">
+                      <a class="dcjq-parent" href="javascript:;">
+                          <i class="fa fa-user"></i>
+                          <span>Students</span>
+                        <span class="dcjq-icon"></span></a>
+                      <ul class="sub" style="display: block;">
+                          <li><a href="view_students.html">View Students</a></li>
+                          <li><a href="add_student.html">Add Student</a></li>
                       </ul>
                   </li>
-                  <li class="sub-menu">
-                      <a href="javascript:;" >
-                          <i class="fa fa-tasks"></i>
-                          <span>Forms</span>
-                      </a>
-                      <ul class="sub">
-                          <li><a  href="form_component.php">Form Components</a></li>
-                      </ul>
-                  </li>
-                  <li class="sub-menu">
-                      <a href="javascript:;" >
-                          <i class="fa fa-th"></i>
-                          <span>Data Tables</span>
-                      </a>
-                      <ul class="sub">
-                          <li><a  href="basic_table.php">Basic Table</a></li>
-                          <li><a  href="responsive_table.php">Responsive Table</a></li>
-                      </ul>
-                  </li>
-                  <li class="sub-menu">
-                      <a href="javascript:;" >
-                          <i class=" fa fa-bar-chart-o"></i>
-                          <span>Charts</span>
-                      </a>
-                      <ul class="sub">
-                          <li><a  href="morris.php">Morris</a></li>
-                          <li><a  href="chartjs.php">Chartjs</a></li>
+
+                     <li class="sub-menu dcjq-parent-li">
+                      <a class="dcjq-parent" href="javascript:;">
+                          <i class="fa fa-male"></i>
+                          <span>Staff</span>
+                        <span class="dcjq-icon"></span></a>
+                      <ul class="sub" style="display: block;">
+                          <li><a href="view_staff.html">View Staff</a></li>
+                          <li><a href="add_staff.html">Add Staff</a></li>
                       </ul>
                   </li>
 
