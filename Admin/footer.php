456fbda7b4eb5e2b46788029b1ae1f7e0c39bf13
Sara Hassan <saraaawwad12@gmail.com>
2018-04-13 14:01:08 +0200

    move files

--- Admin/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="site-footer">
-          <div class="text-center">
-              2018 - NIS
-              <a href="index.php#" class="go-top">
-                  <i class="fa fa-angle-up"></i>
-              </a>
-          </div>
-      </footer>
